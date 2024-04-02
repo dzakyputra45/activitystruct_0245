a8ac30222961fa27b6aa1088635bab9cca9bef83
dzakyputra45 <dzakypratama45@gmail.com>
2024-04-02 15:18:41 +0700

    Nama variable untuk keseluruhan struktur mahasiswa

--- StrukturE_3.cpp
+++ StrukturE_3.cpp
@@ -6,8 +6,37 @@ struct DetailAlamat {
     string Kota;
 };
 
+struct Mahasiswa {
+    string NIM;
+    string Nama;
+    DetailAlamat Alamat;
+    int umur;
+};
+
 int main()
 {
-   
+    Mahasiswa Mhs;
+    cout << "Nomor Mahasiswa : " ;
+    cin >> Mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> Mhs.Nama ;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> Mhs.Alamat.Desa;
+    cout << "\t Nama Kota : ";
+    cin >> Mhs.Alamat.Kota;
+
+    cout << "Umur Mahasiswa :";
+    cin >> Mhs.umur;
+
+
+    cout << endl;
+    cout << "\n NIM : " << Mhs.NIM;
+    cout << "\n Nama : " << Mhs.Nama;
+    cout << "\n Alamat : ";
+    cout << "\n Desa : " << Mhs.Alamat.Desa;
+    cout << "\n Kota : " << Mhs.Alamat.Kota;
+    cout << "\n Umur : " << Mhs.umur;
 
 }
