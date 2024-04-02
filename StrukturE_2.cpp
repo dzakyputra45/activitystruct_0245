b85e8e2e1b8f318a7e70f2fea8b67b3b444b83f7
dzakyputra45 <dzakypratama45@gmail.com>
2024-04-02 14:35:09 +0700

    Membuat fungsi main struktur detail alamat

--- StrukturE_2.cpp
+++ StrukturE_2.cpp
@@ -12,3 +12,29 @@ struct Mahasiswa {
     DetailAlamat Alamat;
     int umur;
 };
+
+int main(){
+    Mahasiswa Mhs;
+    cout << "Nomor Mahasiswa : " ;
+    cin >> Mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> Mhs.Nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> Mhs.Alamat.Desa;
+    cout << "\t Nama Kota : ";
+    cin >> Mhs.Alamat.Kota;
+
+    cout << "Umur Mahasiswa :";
+    cin >> Mhs.umur;
+
+
+    cout << endl;
+    cout << "\n NIM : " << Mhs.NIM;
+    cout << "\n Nama : " << Mhs.Nama;
+    cout << "\n Alamat : "; 
+    cout << "\n \t Desa : " << Mhs.Alamat.Desa;
+    cout << "\n \t Kota : " << Mhs.Alamat.Kota;
+    cout << "\n Umur : " << Mhs.umur;
+}
